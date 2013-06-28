6f14af4abccd6147f576e8bd5ec718c03c3d5674
Gonçalo Silva <gnorsilva@gmail.com>
2013-06-28 08:13:00 +0100

    Matching the Static pages controller to routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  [{title: 'Home',  h1_text: 'Sample App'},
-   {title: 'Help',  h1_text: 'Help!'},
-   {title: 'About', h1_text: 'About Us'},
-   {title: 'Contact', h1_text: 'Contact Us'}
+  [{path: 'root_path', title: 'Home',  h1_text: 'Sample App'},
+   {path: 'help_path', title: 'Help',  h1_text: 'Help!'},
+   {path: 'about_path', title: 'About', h1_text: 'About Us'},
+   {path: 'contact_path', title: 'Contact', h1_text: 'Contact Us'}
   ].each do |values|
 
     describe "#{values[:title]} page" do
-      before { visit "/static_pages/#{values[:title].downcase}" }
+      before { visit eval(values[:path]) }
 
       it { page.should have_title("Ruby on Rails Tutorial Sample App | #{values[:title]}") }
 
